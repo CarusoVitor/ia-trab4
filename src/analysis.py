3cddd73c37cf068f6bb0a8e197d584b85979ea22
Vítor Caruso Rodrigues Ferrer <crfvitor8d@hotmail.com>
2023-03-23 15:16:01 -0300

    q6 done

--- src/analysis.py
+++ src/analysis.py
@@ -65,9 +65,10 @@ def question3e():
     # If not possible, return 'NOT POSSIBLE'
 
 def question6():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    answerEpsilon = 0.2
+    answerLearningRate = 0.5
+    # return answerEpsilon, answerLearningRate
+    return 'NOT POSSIBLE'
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
